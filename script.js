d9d99d0f5715c8acf64a02539ddbc45f732f7210
Samuel Prieto Freitas <45234968+freitassp@users.noreply.github.com>
2024-11-08 07:17:13 -0300

    rtyrty

--- script.js
+++ script.js
@@ -5,3 +5,16 @@ inputCheck.addEventListener('click', () => {
     const modo = inputCheck.checked ? 'dark' : 'light'
     elemento.setAttribute("data-bs-theme", modo)
 })
+
+function enviarFormulario() {
+    // Limpar todos os campos do formulário
+    document.getElementById('floatingNome').value = '';
+    document.getElementById('floatingEmail').value = '';
+    document.getElementById('floatingTelefone').value = '';
+    document.getElementById('nivel-satisfacao').value = '';
+    document.querySelector('.form-select').selectedIndex = 0;
+    document.getElementById('flexCheckDefault').checked = false;
+
+    // Mostrar um popup de agradecimento
+    alert("Obrigado! Logo nossa equipe entrará em contato.");
+  }
