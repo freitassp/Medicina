7fc05adfcc14851bc874a5566aabd9ab4210f6cd
Samuel Prieto Freitas <45234968+freitassp@users.noreply.github.com>
2024-11-08 08:18:38 -0300

    gdfg

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+const inputCheck = document.querySelector('#modo-noturno')
+const elemento = document.querySelector('body')
+
+inputCheck.addEventListener('click', () => {
+    const modo = inputCheck.checked ? 'dark' : 'light'
+    elemento.setAttribute("data-bs-theme", modo)
+})
+
 function enviarFormulario() {
-    setTimeout(() => {
-      // Limpar todos os campos do formulário
-      document.getElementById('floatingNome').value = '';
-      document.getElementById('floatingEmail').value = '';
-      document.getElementById('floatingTelefone').value = '';
-      document.getElementById('nivel-satisfacao').value = '';
-      document.querySelector('.form-select').selectedIndex = 0;
-      document.getElementById('flexCheckDefault').checked = false;
-  
-      // Mostrar um popup de agradecimento
-      alert("Obrigado! Logo nossa equipe entrará em contato.");
-    }, 0);
-  }
-  
+  setTimeout(() => {
+    // Limpar todos os campos do formulário
+    document.getElementById('floatingNome').value = '';
+    document.getElementById('floatingEmail').value = '';
+    document.getElementById('floatingTelefone').value = '';
+    document.getElementById('nivel-satisfacao').value = '';
+    document.querySelector('.form-select').selectedIndex = 0;
+    document.getElementById('flexCheckDefault').checked = false;
+
+    // Mostrar um popup de agradecimento
+    alert("Obrigado! Logo nossa equipe entrará em contato.");
+  }, 0);
+}
